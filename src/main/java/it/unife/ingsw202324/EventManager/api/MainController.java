421f500cda3264747e3214f223a6384818ca5776
flaviom02 <flavio.marangon@edu.unife.it>
2024-04-19 14:05:52 +0200

    Aggiunti RestConsumer e endpoint callREST da repo del prof

--- src/main/java/it/unife/ingsw202324/EventManager/api/MainController.java
+++ src/main/java/it/unife/ingsw202324/EventManager/api/MainController.java
@@ -3,6 +3,7 @@ package it.unife.ingsw202324.EventManager.api;
 import it.unife.ingsw202324.EventManager.models.Categories;
 import it.unife.ingsw202324.EventManager.models.MyTable;
 import it.unife.ingsw202324.EventManager.services.MyService;
+import it.unife.ingsw202324.EventManager.services.TemplateRestConsumer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,6 +25,11 @@ public class MainController {
 
         /* Chiamata a un servizio che ritorna inserisce 3 dati e ritorna il db */
         return myService.addElements();
+    }
+
+    @RequestMapping("/callREST") /* Annotation per definire il path del metodo (relativo alla classe)  */
+    public String callRest() {
+        return TemplateRestConsumer.callREST("helloMock", null, true);
     }
 
     @RequestMapping("/category") /* Annotation per definire il path del metodo (relativo alla classe)  */
